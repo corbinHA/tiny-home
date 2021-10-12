db8640aa368a93265aed14967e42e7555ff87202
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-10-12 16:56:26 -0500

    changing the way apps are shown

---
+++ apps.js
@@ -0,0 +1,35 @@
+let apps ={
+    app: {
+        name: 'app',
+        color: '#2ED9EB',
+        font: "'Audiowide', 'cursive'",
+    },
+    blog: {
+        name: 'blog',
+        color: '#2ED9EB',
+        font: "'Audiowide', 'cursive'",
+    },
+    shop: {
+        name: 'shop',
+        color: '#2ED9EB',
+        font: "'Audiowide', 'cursive'",
+    },
+    note: {
+        name: 'note',
+        color: '#2ED9EB',
+        font: "'Audiowide', 'cursive'",
+    },
+    art: {
+        name: 'art',
+        color: '#FF66FF',
+    },
+    game: {
+        name: 'game',
+        color: '#f73600',
+    },
+    grave: {
+        name: 'grave',
+        color: '#f00000',
+        background: '#000000'
+    },
+}
--- index.js
+++ index.js
@@ -1,48 +1,57 @@
-const apps = [
-    'app',
-    'blog',
-    'shop',
-    'note',
-    'game',
-    'art',
-    'grave'
-];
+const appList = Object.values(apps)
 
 const display = document.getElementById('display');
 const appTitle = document.getElementById('app-name');
 const options = document.getElementById('options');
 
 const cleanUp = () => {
+    document.body.style.backgroundColor = '#FFFFFF';
+    document.body.style.color = '#000000';
     display.innerHTML = '';
 }
 
-const setDisplay = () => {
-    let selectedApp = apps[0]
-    appTitle.innerHTML = selectedApp;
-    appTitle.style.fontSize = "112px"
-    appTitle.style.color = "#2ED9EB"
-    if (selectedApp === 'art') {
-        cleanUp();
-        appTitle.style.color = "#FF66FF"
-        appTitle.style.fontFamily = "'Fleur De Leah', cursive";
-        appTitle.style.fontSize = "200px"
-        generateArt();
-    } else if (selectedApp === 'game') {
-        cleanUp();
-        appTitle.style.color = "#f73600"
-        appTitle.style.fontFamily = "'Press Start 2P', cursive";
-        appTitle.style.fontSize = "80px"
-        generateGame();
-    } else if (selectedApp === 'grave') {
-        cleanUp();
-        appTitle.style.color = "#f00000"
-        appTitle.style.fontWeight = "800"
-        appTitle.style.fontSize = "200px"
-        appTitle.style.fontFamily = "'Amatic SC', cursive";
-    } else {
-        appTitle.style.fontFamily = "'Audiowide', 'cursive'";
-        cleanUp();
+const render = appName => {
+    cleanUp();
+    switch(appName) {
+        case('game'):
+            return generateGame();
+        case('art'):
+            return generateArt();
+        case('grave'):
+            return generateGrave();
+        default:
+            return
     }
+}
+
+const setDisplay = () => {
+    let selectedApp = appList[0];
+    appTitle.innerHTML = selectedApp.name;
+    appTitle.style.fontSize = "112px";
+    appTitle.style.color = selectedApp.color;
+    render(selectedApp.name);
+    // if (selectedApp === 'art') {
+    //     cleanUp();
+    //     appTitle.style.color = "#FF66FF";
+    //     appTitle.style.fontFamily = "'Fleur De Leah', cursive";
+    //     appTitle.style.fontSize = "200px";
+    //     generateArt();
+    // } else if (selectedApp === 'game') {
+    //     cleanUp();
+    //     appTitle.style.color = "#f73600";
+    //     appTitle.style.fontFamily = "'Press Start 2P', cursive";
+    //     appTitle.style.fontSize = "80px";
+    //     generateGame();
+    // } else if (selectedApp === 'grave') {
+    //     cleanUp();
+    //     appTitle.style.color = "#f00000";
+    //     appTitle.style.fontWeight = "800";
+    //     appTitle.style.fontSize = "200px";
+    //     appTitle.style.fontFamily = "'Amatic SC', cursive";
+    // } else {
+    //     appTitle.style.fontFamily = "'Audiowide', 'cursive'";
+    //     cleanUp();
+    // }
 }
 
 const toggleDark = () => {
@@ -58,15 +67,15 @@ const toggleLight = () => {
 }
 
 const rotateRight= () => {
-    apps.push(apps.shift())
+    appList.push(appList.shift())
     setDisplay();
-    apps[0] === 'grave' ? toggleDark() : toggleLight();
+    appList[0].name === 'grave' ? toggleDark() : toggleLight();
 }
 
 const rotateLeft= () => {
-    apps.unshift(apps.pop())
+    appList.unshift(appList.pop())
     setDisplay();
-    apps[0] === 'grave' ? toggleDark() : toggleLight();
+    appList[0].name === 'grave' ? toggleDark() : toggleLight();
 }
 
 const keyDown = e => {
@@ -76,6 +85,8 @@ const keyDown = e => {
                 break;
             case(37):
                 rotateLeft()
+                break;
+            default:
                 break;
         }
     }
